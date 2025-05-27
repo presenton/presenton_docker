b41e7e7d59b8ea02d0b5436dbe23784850fb1b8f
shiva raj badu <shivabadu5@gmail.com>
2025-05-28 01:16:59 +0545

    Nextjs Base Url Set To NEXT_PUBLIC_FAST_API

--- servers/nextjs/utils/constant.ts
+++ servers/nextjs/utils/constant.ts
@@ -2,8 +2,10 @@
 'use client';
 
 export const getEnv = () => {
+  const baseUrl =  process.env.NEXT_PUBLIC_FAST_API || 'http://localhost:8000';
+
   return {
-    BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000',
+    BASE_URL: baseUrl,
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || 'http://localhost:3000',
     TEMP_DIRECTORY: process.env.TEMP_DIRECTORY || '/tmp',
   };
